c41d35b8351e614eaf2e05f095278e39e7eb7cd0
chrlyz <chrlyz@skiff.com>
2023-11-15 06:15:35 +0000

    Integrate with AuroWallet

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
-import CreatePost from '@/app/create-post'
+'use client';
+
+import CreatePost from '@/app/create-post';
+import { useEffect } from 'react';
 
 export default function Home() {
+
+  useEffect(() => {
+    const mina = (window as any).mina;
+    if (mina == null) {
+      console.log("No Mina!");
+    } else {
+      console.log("Mina!");
+    }
+  })
+
   return (
   <main>
     WrdHom: The auditable social-media platform
     <CreatePost />
   </main>
   )
+  
 }
