a1cf7b17d5c60be2951222a4a6e92f8cadb8f784
chrlyz <chrlyz@skiff.com>
2023-11-15 23:05:31 +0000

    w3up error

--- app/actions.ts
+++ app/actions.ts
@@ -1,28 +1,47 @@
 'use server';
-import { createDirectoryEncoderStream, CAREncoderStream } from 'ipfs-car';
-import { Web3Storage, File } from 'web3.storage';
+import { createDirectoryEncoderStream, CAREncoderStream, Block } from 'ipfs-car';
+import * as Signer from '@ucanto/principal/ed25519';
+import { CarReader } from '@ipld/car';
+import { importDAG } from '@ucanto/core/delegation';
+import * as Client from '@web3-storage/w3up-client';
 
 export async function createPost(formData: FormData) {
-    const post = formData.get('post');
+  const post = formData.get('post');
     
-    let postCID: any;
-    const file = new File([post!.toString()], 'wrdhom');
-    await createDirectoryEncoderStream([file])
-    .pipeThrough(
-      new TransformStream({
-        transform(block, controller) {
-          postCID = block.cid;
-          controller.enqueue(block);
-        },
-      })
-    )
-    .pipeThrough(new CAREncoderStream())
-    .pipeTo(new WritableStream());
+  let postCID: any;
+  const file = new File([post!.toString()], 'wrdhom');
+  await createDirectoryEncoderStream([file])
+  .pipeThrough(
+    new TransformStream({
+      transform(block, controller) {
+        postCID = block.cid;
+        controller.enqueue(block);
+      },
+    })
+  )
+  .pipeThrough(new CAREncoderStream())
+  .pipeTo(new WritableStream());
 
-    console.log(postCID.toString());
+  console.log(postCID.toString());
 
-    const client = new Web3Storage({ token: process.env.WEB3STORAGE_TOKEN! });
-    const storedPostCID = await client.put([file]);
-    
-    console.log(storedPostCID.toString());
+  // Load client with specific private key
+  const principal = Signer.parse(process.env.KEY!);
+  const client = await Client.create({ principal });
+  // Add proof that this agent has been delegated capabilities on the space
+  const proof = await parseProof(process.env.PROOF!);
+  const space = await client.addSpace(proof);
+  await client.setCurrentSpace(space.did());
+
+  const storedPostCID = client.uploadDirectory([file]);
+  
+  console.log(storedPostCID.toString());
+}
+
+async function parseProof (data: string) {
+  const blocks = []
+  const reader = await CarReader.fromBytes(Buffer.from(data, 'base64'))
+  for await (const block of reader.blocks()) {
+    blocks.push(block)
+  }
+  return importDAG(blocks as Iterable<Block<unknown, number, number, 1>>)
 }
