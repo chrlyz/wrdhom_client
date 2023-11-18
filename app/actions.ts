929eae003306a7b0648b70df1edc9d51f6c6b56e
chrlyz <chrlyz@skiff.com>
2023-11-18 09:02:58 +0000

    Save successful submission of signed data

--- app/actions.ts
+++ app/actions.ts
@@ -1,13 +1,14 @@
 'use server';
-import { createDirectoryEncoderStream, CAREncoderStream } from 'ipfs-car';
-import { File } from '@web-std/file';
+import { createFileEncoderStream, CAREncoderStream } from 'ipfs-car';
+import { Blob } from '@web-std/file';
+import { SignedData } from '@aurowallet/mina-provider';
 
-export async function createPost(formData: FormData) {
-    const post = formData.get('post');
+export async function createPost(signedData: SignedData, formData: FormData) {
+    const post = formData.get('post')!.toString().replace("\r", "");
     
     let postCID: any;
-    const file = new File([post!.toString()], 'wrdhom');
-    await createDirectoryEncoderStream([file])
+    const file = new Blob([post!]);
+    await createFileEncoderStream(file)
     .pipeThrough(
       new TransformStream({
         transform(block, controller) {
@@ -19,5 +20,9 @@ export async function createPost(formData: FormData) {
     .pipeThrough(new CAREncoderStream())
     .pipeTo(new WritableStream());
 
+    signedData.data === postCID.toString() ? console.log('YES!!!') : console.log('NO!!!');
+    console.log(signedData.data);
+
+    //post!.toString().split("").map((char) => console.log(char.charCodeAt(0)))
     console.log(postCID.toString());
 }
