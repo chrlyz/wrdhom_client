157617c857b61f6a59db56b2eeced63bbc8564d4
chrlyz <chrlyz@skiff.com>
2023-11-20 08:27:55 +0000

    Implement submission of signed posts by client to an external backend endpoint, with proper UI behavior

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+          {
+            source: "/posts",
+            destination: "http://localhost:3001/posts",
+          }
+        ];
+    }
+}
 
 module.exports = nextConfig
