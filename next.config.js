059ff766bbfa66ec1d86cafdaf0cc104425caaa2
chrlyz <chrlyz@skiff.com>
2023-11-26 03:50:34 +0000

    Set proper headers for using o1js on the browser

--- next.config.js
+++ next.config.js
@@ -8,6 +8,23 @@ const nextConfig = {
             destination: "http://localhost:3001/posts",
           }
         ];
+    },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Cross-Origin-Opener-Policy',
+              value: 'same-origin',
+            },
+            {
+              key: 'Cross-Origin-Embedder-Policy',
+              value: 'require-corp',
+            },
+          ],
+        },
+      ];
     }
 }
 
