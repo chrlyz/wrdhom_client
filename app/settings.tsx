afead398ec099bd0d6e3edd42057f2918799defe
chrlyz <chrlyz@skiff.com>
2023-12-22 03:57:38 +0000

    Implement UI to set block range for publication of posts, and audit range in response

--- app/settings.tsx
+++ app/settings.tsx
@@ -5,11 +5,19 @@ export default function AppSettings({
     showSettings,
     howManyPosts,
     setHowManyPosts,
+    fromBlock,
+    setFromBlock,
+    toBlock,
+    setToBlock
 }: {
     visibleSettings: boolean,
     showSettings: () => void,
     howManyPosts: number,
     setHowManyPosts: Dispatch<SetStateAction<number>>,
+    fromBlock: number,
+    setFromBlock: Dispatch<SetStateAction<number>>,
+    toBlock: number,
+    setToBlock: Dispatch<SetStateAction<number>>
 }) {
     return (
         <div className="p-4">
@@ -18,12 +26,30 @@ export default function AppSettings({
             </button>
             {visibleSettings && (
                 <div className="text-s">
-                    <label className="p-2">Posts per request:</label>
-                    <input className="text-right border-4 shadow-md" type="number"
-                        defaultValue={howManyPosts}
-                        onChange={e => setHowManyPosts(Number(e.target.value))}
-                        min={1} max={100}>
-                    </input>
+                    <div className="mt-4">
+                        <label className="p-2">Posts per request:</label>
+                        <input className="text-right border-4 shadow-md" type="number"
+                            defaultValue={howManyPosts}
+                            onChange={e => setHowManyPosts(Number(e.target.value))}
+                            min={1} max={100}>
+                        </input>
+                    </div>
+                    <div className="mt-4">
+                        <label className="p-2">From block:</label>
+                        <input className="text-right border-4 shadow-md" type="number"
+                            defaultValue={fromBlock}
+                            onChange={e => setFromBlock(Number(e.target.value))}
+                            min={24_402} max={1000_000_000}>
+                        </input>
+                    </div>
+                    <div className="mt-4">
+                        <label className="p-2">To block:</label>
+                        <input className="text-right border-4 shadow-md" type="number"
+                            defaultValue={toBlock}
+                            onChange={e => setToBlock(Number(e.target.value))}
+                            min={24_403} max={1000_000_000}>
+                        </input>
+                    </div>
                 </div>
             )}
         </div>
