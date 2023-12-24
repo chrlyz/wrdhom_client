df22fdae6dfbb83eeca12c0fdbe7ce548546cac7
chrlyz <chrlyz@skiff.com>
2023-12-24 06:52:26 +0000

    Handle case when a query returns zero posts

--- app/get-profile.tsx
+++ app/get-profile.tsx
@@ -25,12 +25,14 @@ export default function GetProfile({
   const [errorMessage, setErrorMessage] = useState(null);
   const [warningMessage, setWarningMessage] = useState(null);
   const [triggerAudit, setTriggerAudit] = useState(false);
+  const [whenZeroPosts, setWhenZeroPosts] = useState(false);
 
   const fetchPosts = async () => {
     try {
       setLoading(true);
       setErrorMessage(null);
       setWarningMessage(null);
+      setWhenZeroPosts(false);
       const response = await fetch(`/profile`+
       `?posterAddress=${profilePosterAddress}`+
       `&howMany=${profileHowManyPosts}`+
@@ -43,6 +45,11 @@ export default function GetProfile({
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
+      const data: any[] = await response.json();
+      if (data.length === 0) {
+        setLoading(false);
+        setWhenZeroPosts(true);
+      }
       const { MerkleMapWitness, fetchAccount } = await import('o1js');
       const { PostState } = await import('wrdhom');
       const postsContractData = await fetchAccount({
@@ -50,7 +57,6 @@ export default function GetProfile({
       }, '/graphql');
       const fetchedPostsRoot = postsContractData.account?.zkapp?.appState[2].toString();
       console.log('fetchedPostsRoot: ' + fetchedPostsRoot);
-      const data: any[] = await response.json();
 
       // Remove post to cause a gap error
       //data.splice(2, 1);
@@ -173,6 +179,11 @@ export default function GetProfile({
             </div>
         );
       })}
+      {whenZeroPosts && <div className="p-2 border-b-2 shadow-lg">
+        <div className="flex items-center border-4 p-2 shadow-lg whitespace-pre-wrap break-all">
+            <p >The query threw zero posts</p>
+        </div>
+      </div>}
     </div>
   );
 };
