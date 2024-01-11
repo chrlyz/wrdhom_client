39b09e224082f1d3c2c06be5dda72e5a05b7f50b
chrlyz <chrlyz@skiff.com>
2024-01-11 04:19:39 +0000

    Change message for installing wallet

--- app/install-wallet.tsx
+++ app/install-wallet.tsx
@@ -2,7 +2,7 @@ export default function InstallWallet() {
   return (
     <a href="https://aurowallet.com"
       className="hover:underline">
-      Install wallet to be able to post
+      Install wallet to be able to post, react, comment, and repost n_n
     </a>
   )
 }
