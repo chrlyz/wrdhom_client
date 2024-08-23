bca0ca2ca1adf9beb7f10dff1390b5491be9c54c
chrlyz <chrlyzxchrlyz@gmail.com>
2024-08-23 04:26:19 +0000

    Add delete post and repost interfaces, to the post showed at the top on the comments feed

--- app/components/feeds/get-global-feed.tsx
+++ app/components/feeds/get-global-feed.tsx
@@ -10,6 +10,13 @@ import CreatePost from '../posts/create-post';
 import DeleteButton from '../posts/delete-post-button';
 import DeleteRepostButton from '../reposts/delete-repost-button';
 import { CommentState, PostState, ReactionState, RepostState } from 'wrdhom';
+import {
+  EmbeddedReactions,
+  ProcessedPosts,
+  ProcessedReposts,
+  EmbeddedComments,
+  EmbeddedReposts
+} from '../../types';
 
 export default function GetGlobalFeed({
   getGlobalFeed,
@@ -874,71 +881,4 @@ export default function GetGlobalFeed({
       </div>}
     </div>
   );
-};
-
-export type EmbeddedReactions = {
-  reactionState: any,
-  reactionWitness: JSON,
-  reactionEmoji: string
-};
-
-export type EmbeddedComments = {
-  commentState: JSON,
-  commentWitness: JSON
-}
-
-export type EmbeddedReposts = {
-  repostState: JSON,
-  repostWitness: JSON
-}
-
-export type ProcessedPosts = {
-  postState: JSON,
-  postWitness: JSON,
-  postKey: string,
-  postContentID: string,
-  content: string,
-  shortPosterAddressEnd: string,
-  allEmbeddedReactions: EmbeddedReactions[],
-  filteredEmbeddedReactions: EmbeddedReactions[],
-  top3Emojis: string[],
-  numberOfReactions: number,
-  numberOfReactionsWitness: JSON,
-  embeddedComments: EmbeddedComments[],
-  numberOfComments: number,
-  numberOfCommentsWitness: JSON,
-  numberOfNonDeletedComments: Number,
-  embeddedReposts: EmbeddedReposts[],
-  numberOfReposts: number,
-  numberOfRepostsWitness: JSON,
-  numberOfNonDeletedReposts: Number,
-  currentUserRepostState: JSON | undefined,
-  currentUserRepostKey: string | undefined,
-  currentUserRepostWitness: JSON | undefined,
-};
-
-export type ProcessedReposts = {
-  repostState: JSON,
-  repostWitness: JSON,
-  repostKey: string,
-  shortReposterAddressEnd: string,
-  postState: JSON,
-  postWitness: JSON,
-  postKey: string,
-  postContentID: string,
-  content: string,
-  shortPosterAddressEnd: string,
-  allEmbeddedReactions: EmbeddedReactions[],
-  filteredEmbeddedReactions: EmbeddedReactions[],
-  top3Emojis: string[],
-  numberOfReactions: number,
-  numberOfReactionsWitness: JSON,
-  embeddedComments: EmbeddedComments[],
-  numberOfComments: number,
-  numberOfCommentsWitness: JSON,
-  numberOfNonDeletedComments: Number,
-  embeddedReposts: EmbeddedReposts[],
-  numberOfReposts: number,
-  numberOfRepostsWitness: JSON,
-  numberOfNonDeletedReposts: Number
 };
---
+++ app/types.ts
@@ -0,0 +1,66 @@
+export type EmbeddedReactions = {
+    reactionState: any,
+    reactionWitness: JSON,
+    reactionEmoji: string
+  };
+  
+  export type EmbeddedComments = {
+    commentState: JSON,
+    commentWitness: JSON
+  }
+  
+  export type EmbeddedReposts = {
+    repostState: JSON,
+    repostWitness: JSON
+  }
+  
+  export type ProcessedPosts = {
+    postState: JSON,
+    postWitness: JSON,
+    postKey: string,
+    postContentID: string,
+    content: string,
+    shortPosterAddressEnd: string,
+    allEmbeddedReactions: EmbeddedReactions[],
+    filteredEmbeddedReactions: EmbeddedReactions[],
+    top3Emojis: string[],
+    numberOfReactions: number,
+    numberOfReactionsWitness: JSON,
+    embeddedComments: EmbeddedComments[],
+    numberOfComments: number,
+    numberOfCommentsWitness: JSON,
+    numberOfNonDeletedComments: Number,
+    embeddedReposts: EmbeddedReposts[],
+    numberOfReposts: number,
+    numberOfRepostsWitness: JSON,
+    numberOfNonDeletedReposts: Number,
+    currentUserRepostState: JSON | undefined,
+    currentUserRepostKey: string | undefined,
+    currentUserRepostWitness: JSON | undefined,
+  };
+  
+  export type ProcessedReposts = {
+    repostState: JSON,
+    repostWitness: JSON,
+    repostKey: string,
+    shortReposterAddressEnd: string,
+    postState: JSON,
+    postWitness: JSON,
+    postKey: string,
+    postContentID: string,
+    content: string,
+    shortPosterAddressEnd: string,
+    allEmbeddedReactions: EmbeddedReactions[],
+    filteredEmbeddedReactions: EmbeddedReactions[],
+    top3Emojis: string[],
+    numberOfReactions: number,
+    numberOfReactionsWitness: JSON,
+    embeddedComments: EmbeddedComments[],
+    numberOfComments: number,
+    numberOfCommentsWitness: JSON,
+    numberOfNonDeletedComments: Number,
+    embeddedReposts: EmbeddedReposts[],
+    numberOfReposts: number,
+    numberOfRepostsWitness: JSON,
+    numberOfNonDeletedReposts: Number
+  };
